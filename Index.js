b1095beaea8cecd6d555fee7bc0d33958fd5dd82
KNein <Kimackman0311@gmail.com>
2025-02-18 02:24:17 +0100

    Presence status

--- Index.js
+++ Index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
 
@@ -6,10 +6,24 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    
     client.guilds.cache.forEach(guild => {
-        console.log(`Connected to server: ${guild.name}`);
+        console.log(`Connected to server: ${guild.name} with ID: ${guild.id}`);
+    });
+    
+// Presence status of the bot
+    client.user.setPresence({
+        activities: [{
+            name: '/fenno_stinks',
+            type: ActivityType.Streaming,
+            url: 'https://twitch.tv/K_Nein_200'
+        }],
+        status: 'online'
     });
 });
+
+
+
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
@@ -22,13 +36,13 @@ client.on('interactionCreate', async interaction => {
     let commandCounts;
     try {
         commandCounts = JSON.parse(fs.readFileSync('./commandCounts.json', 'utf8'));
+        console.log('Successfully read commandCounts.json');
     } catch (error) {
         console.error('Error reading commandCounts.json:', error);
         return;
     }
 
     if (commandName === "fenno_stinks") {  
-
         console.log(`Processing "fenno_stinks" command for user: ${user.username}`);
 
         // Check if the user ID is new
@@ -50,11 +64,8 @@ client.on('interactionCreate', async interaction => {
             await interaction.reply(`${user.username} voted that <@548476592083697675> is a stinky fox!`);
         } else {
             await interaction.reply("You already voted");
-        } // new command/responses
-
-    }
-    
-    else if (commandName === "yes") {  
+        }
+    } else if (commandName === "yes") {  
         console.log(`Processing "yes" command for user: ${user.username}`);
         await interaction.reply(`No`);
     }
